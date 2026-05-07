d32242ca7d6122e8ba0ba721fdfa59803f1bb47d
Valeriia Danylo <159473200+Valeriia-Danilo@users.noreply.github.com>
2026-05-07 14:00:07 +0200

    fix pege about, hero, swiper cards

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,10 @@
-import Container from '@/components/Container/Container';
 import About from '@/components/About/About';
 
 const AboutPage = () => {
   return (
-    <Container>
+    <>
       <About />
-    </Container>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Container from '@/components/Container/Container';
 import Hero from '@/components/Hero/Hero';
 export default function Home() {
   return (
-    <Container>
+    <>
       <Hero />
-    </Container>
+    </>
   );
 }
